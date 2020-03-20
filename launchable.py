36591bd8a3e612346d4dfb33b635436141251f08
Dallolz <luc.semon@epitech.eu>
2020-03-20 14:12:33 +0100

    wow

--- launchable.py
+++ launchable.py
@@ -1,47 +1,59 @@
-# import the spiders you want to run
-from spiders.similartechSpider import similartechSpider
-from spiders.addonsimilartechSpider import addonsimilartechSpider
+# # import the spiders you want to run
+# from spiders.similartechSpider import SimilarTechSpider
+# from spiders.addonsimilartechSpider import AddonSimilarSpider
 
-# scrapy api imports
-from scrapy import signals, log
-from twisted.internet import reactor
-from scrapy.crawler import Crawler
-from scrapy.settings import Settings
+# # scrapy api imports
+# from scrapy import signals, log
+# from twisted.internet import reactor
+# from scrapy.crawler import Crawler
+# from scrapy.settings import Settings
 
 
-# list of crawlers
-TO_CRAWL = [similartechSpider, addonsimilartechSpider]
+# # list of crawlers
+# TO_CRAWL = [similartechSpider, addonsimilartechSpider]
 
-# crawlers that are running 
-RUNNING_CRAWLERS = []
+# # crawlers that are running 
+# RUNNING_CRAWLERS = []
 
-def spider_closing(spider):
-    """
-    Activates on spider closed signal
-    """
-    log.msg("Spider closed: %s" % spider, level=log.INFO)
-    RUNNING_CRAWLERS.remove(spider)
-    if not RUNNING_CRAWLERS:
-        reactor.stop()
+# def spider_closing(spider):
+#     """
+#     Activates on spider closed signal
+#     """
+#     log.msg("Spider closed: %s" % spider, level=log.INFO)
+#     RUNNING_CRAWLERS.remove(spider)
+#     if not RUNNING_CRAWLERS:
+#         reactor.stop()
 
-# start logger
-log.start(loglevel=log.DEBUG)
+# # start logger
+# log.start(loglevel=log.DEBUG)
 
-# set up the crawler and start to crawl one spider at a time
-for spider in TO_CRAWL:
-    settings = Settings()
+# # set up the crawler and start to crawl one spider at a time
+# for spider in TO_CRAWL:
+#     settings = Settings()
 
-    # crawl responsibly
-    settings.set("USER_AGENT", "Dallolz (+http://dallolz.github.io)")
-    crawler = Crawler(settings)
-    crawler_obj = spider()
-    RUNNING_CRAWLERS.append(crawler_obj)
+#     # crawl responsibly
+#     settings.set("USER_AGENT", "Dallolz (+http://dallolz.github.io)")
+#     crawler = Crawler(settings)
+#     crawler_obj = spider()
+#     RUNNING_CRAWLERS.append(crawler_obj)
 
-    # stop reactor when spider closes
-    crawler.signals.connect(spider_closing, signal=signals.spider_closed)
-    crawler.configure()
-    crawler.crawl(crawler_obj)
-    crawler.start()
+#     # stop reactor when spider closes
+#     crawler.signals.connect(spider_closing, signal=signals.spider_closed)
+#     crawler.configure()
+#     crawler.crawl(crawler_obj)
+#     crawler.start()
 
-# blocks process; so always keep as the last statement
-reactor.run()
+# # blocks process; so always keep as the last statement
+# reactor.run()
+
+import scrapy
+from scrapy.crawler import CrawlerProcess
+
+import os
+print(os.getcwd())
+
+from script_v1.spiders import AddonSimilarSpider, SimilarTechSpider                                                     
+process = CrawlerProcess()
+process.crawl(SimilarTechSpider)
+process.crawl(AddonSimilarSpider)
+process.start()
--- settings.py
+++ settings.py
@@ -33,7 +33,7 @@ USER_AGENT_LIST = ['Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTM
                    'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:28.0) Gecko/20100101 Firefox/28.0']
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -41,7 +41,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+# DOWNLOAD_DELAY = 4
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
